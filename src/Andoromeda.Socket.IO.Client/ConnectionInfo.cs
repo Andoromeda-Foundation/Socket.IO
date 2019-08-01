f16121a26ad907be7d1ebd143ae86ed3f7c5d558
神樹桜乃 <kodama@sakuno.moe>
2019-08-01 16:59:04 +0800

    Remove simple json since all TFMs support System.Text.Json

--- src/Andoromeda.Socket.IO.Client/ConnectionInfo.cs
+++ src/Andoromeda.Socket.IO.Client/ConnectionInfo.cs
@@ -1,29 +1,29 @@
-﻿#if NETSTANDARD2_1
+﻿using System;
+using System.Text.Json;
 using System.Text.Json.Serialization;
-#endif
 
 namespace Andoromeda.Socket.IO.Client
 {
     sealed partial class ConnectionInfo
     {
-#if NETSTANDARD2_1
         [JsonPropertyName("sid")]
-#endif
         public string SocketId { get; set; }
 
-#if NETSTANDARD2_1
         [JsonPropertyName("upgrades")]
-#endif
         public string[] Upgrades { get; set; }
 
-#if NETSTANDARD2_1
         [JsonPropertyName("pingInterval")]
-#endif
         public int PingInterval { get; set; }
 
-#if NETSTANDARD2_1
         [JsonPropertyName("pingTimeout")]
-#endif
         public int PingTimeout { get; set; }
+
+        public static ConnectionInfo Parse(ReadOnlySpan<byte> content)
+        {
+            if (content[0] != '0')
+                Utils.ThrowParseException();
+
+            return JsonSerializer.Deserialize<ConnectionInfo>(content.Slice(1));
+        }
     }
 }
